5bcc133ba7ece952f89124b01bd3d45b60d9523a
Cal Courtney <calthejuggler@gmail.com>
2025-04-04 18:36:00 +0100

    chore: clean test filenames

--- test/context_test.dart
+++ test/context_test.dart
@@ -20,7 +20,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'context_2_test.mocks.dart';
+import 'context_test.mocks.dart';
 import 'test_utils.dart';
 
 @GenerateNiceMocks([
