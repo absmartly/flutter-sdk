82e7761249e06ee260fd952ed6c202ba510d4577
Jonas Alves <jonas@absmartly.com>
2026-01-20 19:20:56 +0000

    feat(flutter_sdk): create main export file re-exporting absmartly_dart

---
+++ packages/flutter_sdk/lib/absmartly_sdk.dart
@@ -0,0 +1,8 @@
+library absmartly_sdk;
+
+// Re-export the entire Dart SDK
+export 'package:absmartly_dart/absmartly_dart.dart';
+
+// Flutter-specific exports (to be added later)
+// export 'src/absmartly_provider.dart';
+// export 'src/experiment_builder.dart';
